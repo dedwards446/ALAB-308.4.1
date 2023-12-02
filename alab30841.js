a2c3674bf46408f6f2386807e5c57cdc7e9fe88a
dedwards446 <150183824+dedwards446@users.noreply.github.com>
2023-12-01 21:33:53 -0600

    second commit
    updated with part 3

--- alab30841.js
+++ alab30841.js
@@ -58,11 +58,11 @@ let csvString = ("ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19
 
 
  // Split the CSV string into rows based on newline characters.
-let rows = csvString.split("\n");
+let rows = csvString.split('\n');
 for (let i = 0; i < rows.length; i++) {
   // Split the current row into individual elements (id, name, occupation, age)
  // based on the comma delimiter and assign them to respective variables.
- let [cell1, cell2, cell3, cell4] = rows[i].split(",");
+ let [cell1, cell2, cell3, cell4] = rows[i].split(',');
     console.log(cell1, cell2, cell3, cell4);
 }
 
@@ -81,3 +81,31 @@ for(let line of lines){
 }
 console.log(twoD);
 
+//////////// ALAB 308.4.1
+///////// Part 3
+//////////  Transforming Data
+
+//using const map1 = array1.map((x) => x * 2);
+let header = lines[0].split(',').map(header => header.toLowerCase());;
+let result = [];
+for(let i =1; i<lines.length;i++){
+    let values = lines[i].split(',');
+    let dataObj ={}
+for(let j=0; j< header.length;j++){
+    dataObj[header[j]]= values[j];
+}
+    result.push(dataObj)
+}
+console.log(result.pop());
+console.log('After pop: ',result);
+console.log(result.splice(1, 0,{ id: '48', name: 'Barry', occupation: 'Runner', age: '25' }))
+console.log('After splice: ',result);
+console.log(result.push({ id: '7', name: 'Bilbo', occupation: 'None', age: '111' }));
+console.log('After push: ',result);
+let totalAge = 0;
+for(let i=0;i<result.length;i++){
+    totalAge +=parseInt(result[i].age);
+    console.log('total ages: ',totalAge , result[i].age)
+}
+let avrageAge = totalAge/result.length
+console.log("avrageAge: ",Math.round(avrageAge))
